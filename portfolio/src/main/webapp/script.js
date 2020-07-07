9e5e26b3e5b0a768cf1e132f33d555b4c3659809
shejal00 <f20170122@goa.bits-pilani.ac.in>
2020-07-08 03:31:53 +0530

    add topnavbar

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+// // navbar
+$(function() {
+    $('a[href="#toggle-search"], .navbar-bootsnipp .bootsnipp-search .input-group-btn > .btn[type="reset"]').on('click', function(event) {
+        event.preventDefault();
+        $('.navbar-bootsnipp .bootsnipp-search .input-group > input').val('');
+        $('.navbar-bootsnipp .bootsnipp-search').toggleClass('open');
+        $('a[href="#toggle-search"]').closest('li').toggleClass('active');
+
+        if ($('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
+            /* I think .focus dosen't like css animations, set timeout to make sure input gets focus */
+            setTimeout(function() { 
+                $('.navbar-bootsnipp .bootsnipp-search .form-control').focus();
+            }, 100);
+        }           
+    });
+
+    $(document).on('keyup', function(event) {
+        if (event.which == 27 && $('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
+            $('a[href="#toggle-search"]').trigger('click');
+        }
+    });
+    
+});
+
 /**
  * Adds a random greeting to the page.
  */
